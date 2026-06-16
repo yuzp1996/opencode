a7c598e6b1bc375f1be828dc7626fa20ff1c88b4
yuzhipeng03 <yuzhipeng03@yuzhipeng03deMacBook-Pro.local>
2026-06-16 21:53:42 +0800

    test

--- ts-study/app.js
+++ ts-study/app.js
@@ -1,3 +1,9 @@
 "use strict";
 var message = "hello world";
 console.log(message);
+let age = 10;
+console.log(age);
+function greet(name) {
+    return "hello " + name;
+}
+console.log(greet("yuzhipeng"));
--- ts-study/app.ts
+++ ts-study/app.ts
@@ -1,2 +1,11 @@
 var message = "hello world"
 console.log(message)
+
+let age:number=10
+console.log(age)
+
+function greet(name:string):string{
+    return "hello "+name
+}
+
+console.log(greet("yuzhipeng"))
